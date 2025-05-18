71c8096cf9de32a155a593a94711ea9347584216
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-05-19 00:26:46 +0200

    Checking if we can add string as well.

--- server.py
+++ server.py
@@ -1,18 +1,29 @@
-from flask import Flask
-from random import randrange
-
+from flask import Flask, render_template_string
+import random
 app = Flask(__name__)
+
+random_number =  random.randint(0,9)
+print(f"The secret number is: {random_number}")
+
+@app.route('/<int:guess>')
+def check_guess(guess):
+    if guess > random_number:
+        return f"Too high"
+    elif guess < random_number:
+        return f"Too low"
+    elif guess == random_number:
+        return "This is correct number"
+
+@app.route('/<str:guess1>')
+def check_string(guess1):
+    if guess1 == 'home':
+        return home()
+
 
 @app.route('/')
 def home():
-    number = randrange(9)
     gif_url = "https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif"
     return f'<h1> Guess a number between 0 and 9 </h1><img src="{gif_url}">'
-
-@app.route()
-
-
-
 
 
 
