2190068fdde98cc3e21116b8f527f09aabc14e09
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-05-28 21:32:43 +0200

    Finish higher-lower project.

--- server.py
+++ server.py
@@ -8,16 +8,19 @@ print(f"The secret number is: {random_number}")
 @app.route('/<int:guess>')
 def check_guess(guess):
     if guess > random_number:
-        return f"Too high"
+        gif_url_high = "https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif"
+        return f'<h2>Too high</h2><img src="{gif_url_high}">'
     elif guess < random_number:
-        return f"Too low"
+        gif_url_low = "https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif"
+        return f'<h2>Too low</h2><img src="{gif_url_low}">'
     elif guess == random_number:
-        return "This is correct number"
+        gif_url_correct = "https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif"
+        return f'<h2>Its correct </h2><img src="{gif_url_correct}">'
 
-@app.route('/<str:guess1>')
-def check_string(guess1):
-    if guess1 == 'home':
-        return home()
+# @app.route('/<str:guess1>')
+# def check_string(guess1):
+#     if guess1 == 'home':
+#         return home()
 
 
 @app.route('/')
